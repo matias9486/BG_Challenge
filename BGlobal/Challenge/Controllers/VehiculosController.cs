4d95c2526347bded3b74f63406331d8fa22809bf
matias9486 <matias9486mdp@gmail.com>
2021-09-04 12:32:31 -0300

    Controladora Vehículos y vistas Index y create. También se modificó archivo css y js(para consumir api titulares)

---
+++ BGlobal/Challenge/Controllers/VehiculosController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Challenge.Data;
+using Challenge.Models;
+
+namespace Challenge.Controllers
+{
+    public class VehiculosController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VehiculosController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Vehiculos
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Vehiculos.ToListAsync());
+        }
+
+        
+        // GET: Vehiculos/Create
+        public IActionResult Create()
+        {
+            
+            ViewBag.Marcas = getMarcas();
+            return View();
+        }
+
+        //Metodo creado para crear una lista de marcas y luego mostrarlas en la vista
+        public List<string> getMarcas()
+        {
+            List<string> marcas = new List<string>();
+            marcas.Add("Audi");
+            marcas.Add("Chevrolet");
+            marcas.Add("Fiat");
+            marcas.Add("Ford");
+            marcas.Add("Peugeot");
+
+            return marcas;
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Patente,Marca,Modelo,Puertas,Titular")] Vehiculo vehiculo)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(vehiculo);
+                await _context.SaveChangesAsync();
+
+                TempData["Mensaje"] = "Se agregó vehículo con éxito.";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(vehiculo);
+        }
+
+        
+    }
+}
--- BGlobal/Challenge/wwwroot/js/site.js
+++ BGlobal/Challenge/wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+    $(document).ready(function(){
+
+
+        for (let i = 1; i <= 2; i++) {
+            $.ajax({
+
+                url: "https://reqres.in/api/users?page="+i,
+                type: "get",
+
+                success: function (response) {
+
+                    $.each(response.data, function (i, val) {                        
+                        $("#Titular").append("<option>" + val.last_name + ", " + val.first_name + "</option>");
+                    });
+                }
+            });
+        }
+        
+
+
+});
